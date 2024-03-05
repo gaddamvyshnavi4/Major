dacf014f20d0f196c552c824a80f5a7af21f28a5
gaddamvyshnavi4 <106810453+gaddamvyshnavi4@users.noreply.github.com>
2024-03-05 08:34:50 +0530

    Update language_selector.py

--- language_selector.py
+++ language_selector.py
@@ -1,11 +1,7 @@
 import streamlit as st
 import subprocess
 import pyttsx3
-
-'''
-#welcome to crop yield prediction application
-
-#choose app if you want to get screen in english
-
-#choose tapp if you want to get screen in telugu
-'''
+# Function to convert text to speech
+st.write("welcome to crop yield prediction application")
+st.write("choose app if you want to get screen in english ")
+st.write("choose tapp if you want to get screen in telugu ")

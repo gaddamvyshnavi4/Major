25bbfa993421249f8f9fb18ffd91ec5fdde2c8c3
gaddamvyshnavi4 <106810453+gaddamvyshnavi4@users.noreply.github.com>
2024-03-05 08:34:23 +0530

    Update language_selector.py

--- language_selector.py
+++ language_selector.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import subprocess
 import pyttsx3
-# Function to convert text to speech
-#st.write("welcome to crop yield prediction application")
-#st.write("choose app if you want to get screen in english ")
-#st.write("choose tapp if you want to get screen in telugu ")
+
 '''
 #welcome to crop yield prediction application
+
 #choose app if you want to get screen in english
+
 #choose tapp if you want to get screen in telugu
 '''

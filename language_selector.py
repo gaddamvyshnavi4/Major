a85d1b2bd465e18b21c8503a003870245cb708b2
gaddamvyshnavi4 <106810453+gaddamvyshnavi4@users.noreply.github.com>
2024-03-05 08:38:27 +0530

    Update language_selector.py

--- language_selector.py
+++ language_selector.py
@@ -4,4 +4,4 @@ import pyttsx3
 # Function to convert text to speech
 st.write("welcome to crop yield prediction application")
 st.write("choose app if you want to get screen in english ")
-st.write("choose tapp if you want to get screen in telugu ")
+st.write("మీరు తెలుగులో స్క్రీన్‌ని పొందాలనుకుంటే tappని ఎంచుకోండి ")
